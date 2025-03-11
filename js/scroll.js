e8fa54e5ba5869ab7f0525d6573cc0fe5fac4c21
kwak-jae-yong <dream001211@naver.com>
2025-03-11 15:58:53 +0900

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,32 @@
+const links = document.querySelectorAll('aside .link')
+const sections = document.querySelectorAll('#wrap>section')
+let pageNum = 0
+let totalNum = sections.length
+
+init(pageNum)
+
+
+function init(i){
+    links.forEach((link) => link.classList.remove('active'))
+    sections.forEach((section) => section.classList.remove('current'))
+    
+    links[i].classList.add('active')
+    sections[i].classList.add('current')
+}
+
+links.forEach(function (link, index) {
+
+    link.addEventListener('click', function () {
+
+        init(index)
+
+        gsap.to(window,{
+            duration:1,
+            scrollTo:{
+                y:`#s${index}`
+            }
+        })
+
+    })
+
+})
